d61643baad09bb53af241b85a330d9f07f535fde
KnedlikMCPE <knedlik@trainmazeland.com>
2022-11-16 21:03:48 +0100

    Electrics refactoring

--- Cockpit/Scripts/Systems/electric_system.lua
+++ Cockpit/Scripts/Systems/electric_system.lua
@@ -1,7 +1,7 @@
 dofile(LockOn_Options.script_path.."command_defs.lua")
-dofile(LockOn_Options.script_path.."utilites.lua")
+dofile(LockOn_Options.script_path.."utils.lua")
 
-local electric_system = GetSelf()
+electric_system = GetSelf()
 local dev = electric_system
 
 local update_time_step = 0.02-- 1/0.02 = 50 times per second  
@@ -9,91 +9,101 @@ make_default_activity(update_time_step)
 
 local sensor_data = get_base_data()
 
-function post_initialize()  
 
-   value = 0
-   electric_system:AC_Generator_1_on(value > 0)
-   electric_system:AC_Generator_2_on(value > 0)
-   electric_system:DC_Battery_on(value > 0)
-  
+start_value = 0
+electric_system.AC_Generator_1 = start_value
+electric_system.AC_Generator_2 = start_value
+electric_system.Bat = start_value
+
+function electric_system:set_Bat(value)
+    electric_system.DC_Bat = value
+end
+function electric_system:get_Bat()
+    return electric_system.DC_Bat
+end
+function electric_system:invert_Bat()
+    electric_system.DC_Bat = getOtherValue(electric_system.DC_Bat)
 end
 
-local PARAMETERS = 
-{
-    BATTERY           = get_param_handle("BATTERY"),
-    LGEN              = get_param_handle("LGEN"),
-    RGEN              = get_param_handle("RGEN"),
-}
+function electric_system:set_AC_Generator_2(value)
+    self.AC_Generator_2 = value
+end
+function electric_system:get_AC_Generator_2()
+    return self.AC_Generator_2
+end
+function electric_system:invert_AC_Generator_2()
+    self.DC_Bat = getOtherValue(self.AC_Generator_2)
+end
+
+function electric_system:set_AC_Generator_1(value)
+    self.AC_Generator_1 = value
+end
+function electric_system:get_AC_Generator_1()
+    return self.AC_Generator_1
+end
+function electric_system:invert_AC_Generator_1()
+    self.DC_Bat = getOtherValue(self.AC_Generator_1)
+end
 
 
-afterbunr_state = 0
-afterburne_val = 0
-afterburne_dir = 0.5
+electric_system:listen_command(Keys.PowerOnOff) --listen command needs to be here to work in set command
+electric_system:listen_command(device_commands.BatterySwitchClick)
 
+electric_system:listen_command(Keys.PowerGeneratorLeft) --listen command needs to be here to work in set command
+electric_system:listen_command(device_commands.GenLeftSwitch)
 
-local PowerOnOff        = Keys.PowerOnOff --setting the iCommands from command defs 
-local battery_click     = device_commands.BatterySwitchClick--Track mouse clicks
-
-local LeftGen           = Keys.PowerGeneratorLeft --setting the iCommands from command defs 
-local lgen_click        = device_commands.GenLeftSwitch--Track mouse clicks
-
-local RightGen          = Keys.PowerGeneratorRight --setting the iCommands from command defs 
-local rgen_click        = device_commands.GenRightSwitch--Track mouse clicks
-
-electric_system:listen_command(PowerOnOff) --listen command needs to be here to work in set command
-electric_system:listen_command(battery_click)
-
-electric_system:listen_command(LeftGen) --listen command needs to be here to work in set command
-electric_system:listen_command(lgen_click)
-
-electric_system:listen_command(RightGen) --listen command needs to be here to work in set command
-electric_system:listen_command(rgen_click)
-
-battery_state = 0 -- I tend to make local vars all lower case
-left_gen_state = 0 
-right_gen_state = 0 
+electric_system:listen_command(Keys.PowerGeneratorRight) --listen command needs to be here to work in set command
+electric_system:listen_command(device_commands.GenRightSwitch)
 ------------------------------------------------------------------FUNCTION-POST-INIT---------------------------------------------------------------------------------------------------
 function post_initialize()
 	-- print_message_to_user("Electrical System Initialize")
     local birth = LockOn_Options.init_conditions.birth_place
 
     if birth == "GROUND_HOT" or birth == "AIR_HOT" then
-        battery_state = 1
-        right_gen_state = 1
-        left_gen_state = 1
+        electric_system:set_Bat(1)
+        electric_system:set_AC_Generator_1(1)
+        electric_system:set_AC_Generator_2(1)
     end
 end
 ------------------------------------------------------------FUNCTION-SETCOMMAND---------------------------------------------------------------------------------------------------
 function SetCommand(command,value)
-    if command == PowerOnOff then
-        battery_state = getOtherValue(battery_state)
+    if command == Keys.PowerOnOff then
+        --electric_system:invert_DC_Bat()
+        electric_system:invert_Bat()
     end
 
-    if command == battery_click then
-        dispatch_action(nil,PowerOnOff)-- this is used to push a keyboad/icommand with a mouse click
+    if command == device_commands.BatterySwitchClick then
+        dispatch_action(nil,Keys.PowerOnOff)-- this is used to push a keyboad/icommand with a mouse click
     end
     
-    if command == LeftGen then
-        left_gen_state = get_otherValue(left_gen_state)
+    if command == Keys.PowerGeneratorLeft then
+        electric_system:invert_AC_Generator_1()
     end
 
-    if command == lgen_click then
+    if command == device_commands.GenLeftSwitch then
         dispatch_action(nil,LeftGen)-- this is used to push a keyboad/icommand with a mouse click
     end
 
-    if command == RightGen then
-        right_gen_state = getOtherValue(right_gen_state)
+    if command == Keys.PowerGeneratorRight then
+        electric_system:invert_AC_Generator_2()
     end
 
-    if command == rgen_click then
+    if command == device_commands.GenRightSwitch then
         dispatch_action(nil,RightGen)-- this is used to push a keyboad/icommand with a mouse click
     end
 end--END SETCOMMAND
 ------------------------------------------------------------------FUNCTION-UPDATE---------------------------------------------------------------------------------------------------
+local PARAMETERS = 
+{
+    BATTERY           = get_param_handle("BATTERY"),
+    ACGEN1            = get_param_handle("ACGEN1"),
+    ACGEN2            = get_param_handle("ACGEN2"),
+}
+
 function update()
-    PARAMETERS.BATTERY:set(battery_state)--this will set the BATTERY param to the battery_state local variable # so 0 or 1
-    PARAMETERS.RGEN:set(right_gen_state)
-    PARAMETERS.LGEN:set(left_gen_state)
+    PARAMETERS.BATTERY:set(electric_system.Bat)--this will set the BATTERY param to the battery_state local variable # so 0 or 1
+    PARAMETERS.ACGEN1:set(electric_system:get_AC_Generator_1())
+    PARAMETERS.ACGEN2:set(electric_system:get_AC_Generator_2())
 
 end --END UPDATE
 
