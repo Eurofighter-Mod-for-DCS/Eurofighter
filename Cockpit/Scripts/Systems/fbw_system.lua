94a33a5d36dfca2d0dc171c4e4e3a445b113473d
Gonzalo de santa ana <55592800+gonano4@users.noreply.github.com>
2022-11-17 16:53:00 +0100

    Add files via upload

--- Cockpit/Scripts/Systems/fbw_system.lua
+++ Cockpit/Scripts/Systems/fbw_system.lua
@@ -12,6 +12,7 @@ local sensor_data = get_base_data()
 local ROLL_INPUT = 0
 local PITCH_INPUT = 0
 local RUDDER_INPUT = 0
+local THROTTLE_INPUT = 0
 
 local GERROR = 0
 local T = 0
@@ -48,6 +49,7 @@ local ROLL_OUTPUT = get_param_handle("CURRENT_ROLL_OUTPUT")
 local YAW_OUTPUT = get_param_handle("CURRENT_YAW_OUTPUT")
 
 local PITCH_PAST = 0
+local GTARGET = 0
 
 local Kp = 0
 local Kd = 0 
@@ -65,20 +67,20 @@ FBW:listen_command(2001)--Pitch axis
 FBW:listen_command(2002)--roll
 FBW:listen_command(2003)--rudder/yaw
 
+FBW:listen_command(10061)--Pitch axis
+FBW:listen_command(10062)--roll
+FBW:listen_command(RUDDER_INPUT)--rudder/yaw
+FBW:listen_command(THROTTLE_INPUT)--throttle
+
 function SetCommand(command,value)
-	if command == 2001 then
+	if command == 10061 then
         PITCH_INPUT = value
-    else
-        PITCH_INPUT = 0
-		--print_message_to_user(value)
 	end
-	if command == 2002 then
+	if command == 10062 then
 		ROLL_INPUT = value
-    else
-        ROLL_INPUT = 0
 	end
-	if command == 2003 then
-		RUDDER_INPUT = value	
+	if command == RUDDER_INPUT then
+		RUDDER_OUTPUT = value	
     else
         RUDDER_INPUT = 0	
 	end
@@ -96,10 +98,17 @@ function SetCommand(command,value)
 end
 
 function Sensor_data() 
-	PITCH = sensor_data.getPitch() * 57.29577951308233
-    ROLL = sensor_data.getRoll() * 57.29577951308233
-    GZ = sensor_data.getVerticalAcceleration()
-    GERROR = 1 - GZ
+
+	PITCH = sensor_data.getPitch()
+    ROLL = sensor_data.getRoll()
+    NY = sensor_data.getVerticalAcceleration()
+    NX = sensor_data.getHorizontalAcceleration()
+    NZ = sensor_data.getLateralAcceleration()
+    ROLLRATE = sensor_data.getRateOfRoll()* 57.29577951308233
+    PITCH_RATE = sensor_data.getRateOfPitch()* 57.29577951308233
+    YATRATE = sensor_data.getRateOfYaw()* 57.29577951308233
+    GTARGET = (1 + PITCH_INPUT* 100 / 12.5)
+    GERROR = GTARGET - NY
     PITCH_ERROR = PITCH_TARGET - PITCH
     ROLL_ERROR = ROLL_TARGET - ROLL
     ias_knots = sensor_data.getIndicatedAirSpeed() * 1.94384
@@ -109,8 +118,6 @@ function time_counter()
     T = T + update_time_step
     if T == 1 then
         T = 0
-        PITCH_PAST = PITCH_ERROR
-        -- dispatch_action(nil,97)
     end
 end
 
@@ -122,95 +129,29 @@ function update()
 
     Sensor_data()
     
-    -- local i = 100
-    -- for i =  100, 1, -1 do 
-    --     dispatch_action(nil, 957)
 
-    -- end
+    -- print_message_to_user("EUROFIGHTER PITCH OUTPUT"..GERROR / GTARGET)
 
-    local PITCH_DELTA = PITCH_ERROR - PITCH_PAST
+    --EN PRINCIPIO ASÍ VA BIEN
+    -- ESTABLECER MODOS.
 
-    local Kp = PITCH_ERROR/(10 * ias_knots)
-    local kd = 0 --PITCH_ERROR * update_time_step / 10
-    local Ki = 0
-
-    PITCH_TARGET = PITCH_INPUT
-
-    PITCH_Diff1 = PITCH_TARGET - PITCH_STATE  			-- Difference between desired and current angle
-    PITCH_Diff2 = PITCH_STATE - PITCH_TARGET				-- Difference between current and desired angle
-    -- dispatch_action(nil, Keys.PlaneTrimCancel)
-    WOW = sensor_data.getWOW_NoseLandingGear()
-
-    -- if ROLL_INPUT + PITCH_INPUT <= 0.01 and ROLL_INPUT + PITCH_INPUT >= -0.01  and ROLL < 60 and ROLL > -60 and PITCH < 70 and PITCH > -50 and WOW == 0 then
-    --     if T == update_time_step * 2 then
-    --         PITCH_TARGET = PITCH
-    --     end
-
-    --     if PITCH_ERROR > 0 then 
-    --         dispatch_action(nil, 2019, Kp + Kd + Ki)
-    --         -- print_message_to_user("DOWN")
-    --     elseif PITCH_ERROR < 0 then
-    --         dispatch_action(nil, 2019, Kp + Kd + Ki)  
-    --         -- print_message_to_user("UP")
-    --     elseif PITCH_ERROR == 0 then
-    --         -- print_message_to_user("STABLE")
-    --     end
-    --     -- dispatch_action(nil,97)
-    -- elseif PITCH_INPUT == 0 and WOW == 0 and ROLL > 60 and ROLL < -60 then
-    --     if GERROR < 0 then 
-    --         dispatch_action(nil, 2019, 10*GERROR/ias_knots + Kd + Ki)
-    --         -- print_message_to_user("DOWN")
-    --     elseif GERROR > 0 then
-    --         dispatch_action(nil, 2019, 10*GERROR/ias_knots + Kd + Ki)  
-    --         -- print_message_to_user("UP")
-    --     elseif GERROR == 0 then
-    --         -- print_message_to_user("STABLE")
-    --     end
-    -- elseif PITCH > 70 or PITCH < -50 then
-    --     dispatch_action(nil,2019,0)
-    -- end
-
-    -- local Kp = ROLL_ERROR/(60*ias_knots) 
-    -- local kd = update_time_step / 13
-    -- local Ki = 0
-
-    -- if ROLL_INPUT + PITCH_INPUT <= 0.02 and ROLL_INPUT + PITCH_INPUT >= -0.02  and WOW == 0 and ROLL < 90 and ROLL > -90 and ias_knots<450 then
-    --     if T == update_time_step * 2 then
-    --         ROLL_TARGET = ROLL
-    --     end
-
-    --     if ROLL_ERROR > 0 then 
-    --         dispatch_action(nil, 2020, Kp + Kd + Ki)
-    --         -- print_message_to_user("DERECHA")
-    --     elseif ROLL_ERROR < 0 then
-    --         dispatch_action(nil, 2020, Kp - Kd - Ki)  
-    --         -- print_message_to_user("IZQUIERDA")
-    --     elseif ROLL_ERROR == 0 then
-    --         -- print_message_to_user("STABLE")
-    --     end
-    --     -- dispatch_action(nil,97)
-
-    -- -- end
-    -- print_message_to_user("PITCH ERROR")
-    -- print_message_to_user(PITCH_ERROR)
-
-    -- print_message_to_user("PITCH DELTA")
-    -- print_message_to_user(PITCH_DELTA)
-
-    if PITCH_INPUT+ROLL_INPUT <= -0.03 or PITCH_INPUT+ROLL_INPUT>= 0.03 and WOW == 0 then
-        T = 0 
-        dispatch_action(nil,2019, PITCH_STATE)
-        dispatch_action(nil,2020, 0)
-        PITCH_ERROR = 0
-        ROLL_ERROR = 0
+    if GTARGET < 0 then 
+        PITCH_OUTPUT = -1 * (GERROR / GTARGET)
+    else
+        PITCH_OUTPUT = GERROR / GTARGET
     end
 
---EN PRINCIPIO ASÍ VA BIEN
+    ROLL_OUTPUT = ROLL_INPUT
 
+    if GERROR > 0 then
+        dispatch_action(nil, 2001, PITCH_OUTPUT)	--PITCH
+        -- print_message_to_user(PITCH_OUTPUT)
+    elseif GERROR < 0 then
+        dispatch_action(nil, 2001, PITCH_OUTPUT)	--PITCH
+        -- print_message_to_user(PITCH_OUTPUT)
+    end 
 
-    -- dispatch_action(nil, 174)	--PITCH
-
-    -- dispatch_action(nil, 2002, ROLL_INPUT)	--ROLL
+    dispatch_action(nil, 2002, ROLL_OUTPUT)	--ROLL
 
 
 
