c94b28929589434f1b7014639a7c988d7e36951e
KnedlikMCPE <knedlik@trainmazeland.com>
2022-12-20 21:46:46 +0100

    Device Class

--- Cockpit/Scripts/Systems/electric_system.lua
+++ Cockpit/Scripts/Systems/electric_system.lua
@@ -1,75 +1,42 @@
 dofile(LockOn_Options.script_path.."command_defs.lua")
-dofile(LockOn_Options.script_path.."utils.lua")
+dofile(LockOn_Options.script_path.."device_class.lua")
 
-electric_system = GetSelf()
-local dev = electric_system
+local device = GetSelf()
 
 local update_time_step = 0.02-- 1/0.02 = 50 times per second  
 make_default_activity(update_time_step)
 
 local sensor_data = get_base_data()
 
+electric_system = Device:new()
+electric_system:AddBool("AC_Generator_1")
+electric_system:AddBool("AC_Generator_2")
+electric_system:AddBool("Bat")
 
-start_value = 0
-electric_system.AC_Generator_1 = start_value
-electric_system.AC_Generator_2 = start_value
-electric_system.Bat = start_value
+device:listen_command(Keys.PowerOnOff) --listen command needs to be here to work in set command
+device:listen_command(device_commands.BatterySwitchClick)
 
-function electric_system:set_Bat(value)
-    electric_system.DC_Bat = value
-end
-function electric_system:get_Bat()
-    return electric_system.DC_Bat
-end
-function electric_system:invert_Bat()
-    electric_system.DC_Bat = getOtherValue(electric_system.DC_Bat)
-end
+device:listen_command(Keys.PowerGeneratorLeft) --listen command needs to be here to work in set command
+device:listen_command(device_commands.GenLeftSwitch)
 
-function electric_system:set_AC_Generator_2(value)
-    self.AC_Generator_2 = value
-end
-function electric_system:get_AC_Generator_2()
-    return self.AC_Generator_2
-end
-function electric_system:invert_AC_Generator_2()
-    self.DC_Bat = getOtherValue(self.AC_Generator_2)
-end
-
-function electric_system:set_AC_Generator_1(value)
-    self.AC_Generator_1 = value
-end
-function electric_system:get_AC_Generator_1()
-    return self.AC_Generator_1
-end
-function electric_system:invert_AC_Generator_1()
-    self.DC_Bat = getOtherValue(self.AC_Generator_1)
-end
-
-
-electric_system:listen_command(Keys.PowerOnOff) --listen command needs to be here to work in set command
-electric_system:listen_command(device_commands.BatterySwitchClick)
-
-electric_system:listen_command(Keys.PowerGeneratorLeft) --listen command needs to be here to work in set command
-electric_system:listen_command(device_commands.GenLeftSwitch)
-
-electric_system:listen_command(Keys.PowerGeneratorRight) --listen command needs to be here to work in set command
-electric_system:listen_command(device_commands.GenRightSwitch)
+device:listen_command(Keys.PowerGeneratorRight) --listen command needs to be here to work in set command
+device:listen_command(device_commands.GenRightSwitch)
 ------------------------------------------------------------------FUNCTION-POST-INIT---------------------------------------------------------------------------------------------------
 function post_initialize()
 	-- print_message_to_user("Electrical System Initialize")
     local birth = LockOn_Options.init_conditions.birth_place
 
     if birth == "GROUND_HOT" or birth == "AIR_HOT" then
-        electric_system:set_Bat(1)
-        electric_system:set_AC_Generator_1(1)
-        electric_system:set_AC_Generator_2(1)
+        electric_system.Bat.Set(1)
+        electric_system.AC_Generator_1.Set(1)
+        electric_system.AC_Generator_2.Set(1)
     end
 end
 ------------------------------------------------------------FUNCTION-SETCOMMAND---------------------------------------------------------------------------------------------------
 function SetCommand(command,value)
     if command == Keys.PowerOnOff then
         --electric_system:invert_DC_Bat()
-        electric_system:invert_Bat()
+        electric_system.Bat:Invert()
     end
 
     if command == device_commands.BatterySwitchClick then
@@ -77,7 +44,7 @@ function SetCommand(command,value)
     end
     
     if command == Keys.PowerGeneratorLeft then
-        electric_system:invert_AC_Generator_1()
+        electric_system.AC_Generator_1:Invert()
     end
 
     if command == device_commands.GenLeftSwitch then
@@ -85,7 +52,7 @@ function SetCommand(command,value)
     end
 
     if command == Keys.PowerGeneratorRight then
-        electric_system:invert_AC_Generator_2()
+        electric_system.AC_Generator_2:Invert()
     end
 
     if command == device_commands.GenRightSwitch then
@@ -101,9 +68,9 @@ local PARAMETERS =
 }
 
 function update()
-    PARAMETERS.BATTERY:set(electric_system.Bat)--this will set the BATTERY param to the battery_state local variable # so 0 or 1
-    PARAMETERS.ACGEN1:set(electric_system:get_AC_Generator_1())
-    PARAMETERS.ACGEN2:set(electric_system:get_AC_Generator_2())
+    PARAMETERS.BATTERY:set(electric_system.Bat:Get())--this will set the BATTERY param to the battery_state local variable # so 0 or 1
+    PARAMETERS.ACGEN1:set(electric_system.AC_Generator_1:Get())
+    PARAMETERS.ACGEN2:set(electric_system.AC_Generator_2:Get())
 
 end --END UPDATE
 
